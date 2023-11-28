fc7a84eb7390ec21101643bb3107dfec92569398
autodistries <66792425+autodistries@users.noreply.github.com>
2023-11-28 15:30:49 +0000

    Update script.sh
    changed patch to sed addition for it to work in more cases

--- script.sh
+++ script.sh
@@ -21,29 +21,20 @@ cd ${_srcname}/drivers/media/usb/uvc
 
 
 
-
-patchvalue="--- uvc_driver.old  2023-11-20 11:59:39.000000000 +0100
-+++ uvc_driver.c    2023-11-28 09:52:46.701998741 +0100
-@@ -3102,6 +3102,17 @@
-      .bInterfaceSubClass   = 1,
-      .bInterfaceProtocol   = 0,
-      .driver_info      = UVC_INFO_META(V4L2_META_FMT_D4XX) },
-+     /* Quanta ACER HD User Facing */
-+   { .match_flags      = USB_DEVICE_ID_MATCH_DEVICE
-+               | USB_DEVICE_ID_MATCH_INT_INFO,
-+     .idVendor     = 0x0408,
-+     .idProduct        = 0x4035,
-+     .bInterfaceClass  = USB_CLASS_VIDEO,
-+     .bInterfaceSubClass   = 1,
-+     .bInterfaceProtocol   = UVC_PC_PROTOCOL_15,
-+     .driver_info      = (kernel_ulong_t)&(const struct uvc_device_info){
-+       .uvc_version = 0x010a,
-+     } },
-    /* Generic USB Video Class */
-    { USB_INTERFACE_INFO(USB_CLASS_VIDEO, 1, UVC_PC_PROTOCOL_UNDEFINED) },
-    { USB_INTERFACE_INFO(USB_CLASS_VIDEO, 1, UVC_PC_PROTOCOL_15) },"
-echo "$patchvalue" > diff.patch
-patch uvc_driver.c --ignore-whitespace < diff.patch
+# used to be a patch, but in case the upper lines change, it needs to work
+patchvalue="	  /* Quanta ACER HD User Facing */
+	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
+				| USB_DEVICE_ID_MATCH_INT_INFO,
+	  .idVendor		= 0x0408,
+	  .idProduct		= 0x4035,
+	  .bInterfaceClass	= USB_CLASS_VIDEO,
+	  .bInterfaceSubClass	= 1,
+	  .bInterfaceProtocol	= UVC_PC_PROTOCOL_15,
+	  .driver_info		= (kernel_ulong_t)&(const struct uvc_device_info){
+		.uvc_version = 0x010a,
+	  } },"
+echo "$patchvalue" > patchfile
+sed -i "/\/\* Generic USB Video Class \*\//r patchfile" uvc_driver.c
 
 
 make -j4 -C /lib/modules/$(uname -r)/build M=$(pwd) modules  # complie the updated video modules for your kernel version
